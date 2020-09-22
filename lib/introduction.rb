c744c1debd730b5435234fe5ffbe54c7444702cd
Crystal T <ctalley.cat@gmail.com>
2020-09-22 01:24:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,4 @@
 
-def introduction(name)
-  puts "Hi, my name is #{name}."
-  
-def introduction_with_language(name, language)
-  puts "hi, my name is #{name} and I am learning to program in #{language}."
 
 def introduction_with_language_optional(name, language = "Ruby")
  puts "hi, my name is #{name} and I am learning to program in #{language}."
